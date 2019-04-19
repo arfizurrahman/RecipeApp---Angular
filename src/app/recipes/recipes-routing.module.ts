037e068bf718ae78fb382f0949a8f1cae798c421
Arfizur Rahman <arfizrahman0@gmail.com>
2019-04-19 16:59:40 +0600

    Restructuring services using child injector

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -18,6 +18,7 @@ const recipesRoutes: Routes = [
     imports: [
         RouterModule.forChild(recipesRoutes)
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class RecipesRoutingModule{}
