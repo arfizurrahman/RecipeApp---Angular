d0af3a99d455e04aea9c420b17d80b96e24139ef
Arfizur Rahman <arfizrahman0@gmail.com>
2019-05-01 09:07:44 +0600

    Registering the application store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthGuard } from './auth/auth-gaurd.service';
-import { AuthService } from './auth/auth.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
 
 @NgModule({
   declarations: [
@@ -25,7 +22,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     SharedModule ,
     ShoppingListModule,
     AuthModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
   bootstrap: [AppComponent]
 })
