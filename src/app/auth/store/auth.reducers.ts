b5eed39098d3d8f78e94bbf2238853514233436b
Arfizur Rahman <arfizrahman0@gmail.com>
2019-05-05 22:43:26 +0600

    Added reducer login and actions

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,4 @@
+import * as AuthActions from './auth.actions';
 export interface State {
     token: string;
     authenticated: boolean;
@@ -8,6 +9,21 @@ export interface State {
     authenticated: false
   };
 
-export function authReducer(state = initialState, action) {
-
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch(action.type){
+    case (AuthActions.SIGNUP):
+    case(AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true
+      };
+      case (AuthActions.LOGOUT):
+         return {
+          ...state,
+          token: null,
+          authenticated: false
+         };
+        default: 
+           return state;
+  }
 }
