a54aa204aa924a63bbc222891e409241d145e8ea
Arfizur Rahman <arfizrahman0@gmail.com>
2019-05-06 08:31:43 +0600

    Dispatch actions

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,14 +1,26 @@
+import { Store } from '@ngrx/store';
 import { AuthService } from './../../auth/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../../shared/data-storage.service';
+import * as fromApp from '../../store/app.reducers'
+import * as fromAuth from '../../auth/store/auth.reducers'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  authState: Observable<fromAuth.State>
+
   constructor(private dataStorageService: DataStorageService,
-    private authService: AuthService){}
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>){}
+
+  ngOnInit(){
+    this.authState = this.store.select('auth');
+  }
+
   onSaveData(){
     this.dataStorageService.storeRecipes()
     .subscribe(
