8622811b28b177c6c8468926f7a1b87f471b2c54
Arfizur Rahman <arfizrahman0@gmail.com>
2019-05-17 10:36:24 +0600

    Understanding how effects work

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,10 @@ import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import * as fromApp from '../../store/app.reducers';
+import * as AuthActions from '../store/auth.actions';
+import { Store } from '@ngrx/store';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,7 +13,7 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
   }
@@ -17,6 +21,6 @@ export class SignupComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
     // console.log(email, password);
-    this.authService.signupUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 }
