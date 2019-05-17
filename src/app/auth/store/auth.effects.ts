1cd097a0a300cc5f54131584f073b864cbb97548
Arfizur Rahman <arfizrahman0@gmail.com>
2019-05-17 10:59:05 +0600

    Add auth signin

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -31,5 +31,32 @@ export class AuthEffects {
             }
         ];
     })); 
+
+    @Effect()
+    authSignin = this.actions$
+    .pipe(
+        ofType(AuthActions.TRY_SIGNIN),
+
+    map((action: AuthActions.TrySignin) => {
+        return action.payload;
+    }),
+    switchMap((authData: {username: string, password: string}) => {
+        return from(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password))
+    }),
+    switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken())
+    }),
+    mergeMap((token: string) => {
+        return [
+            {
+                type: AuthActions.SIGNIN
+            },
+            {
+                type: AuthActions.SET_TOKEN,
+                payload: token
+            }
+        ];
+    })); 
+
     constructor(private actions$: Actions){}
 }
